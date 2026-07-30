51286ab177faab825cf2194184d7f4675f83f092
qitongfeng619-ship-it <qitongfeng619@gmail.com>
2026-07-31 01:21:21 +0800

    update sw.js - cache all pages

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
-const CACHE = 'pools-v1';
-const ASSETS = ['/poan/pools.html', '/poan/manifest.webmanifest'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'poan-v2';
+const ASSETS = [
+  '/poan/index.html',
+  '/poan/pools.html',
+  '/poan/stories.html',
+  '/poan/dice.html',
+  '/poan/bodymap.html',
+  '/poan/done-list.html',
+  '/poan/manifest.webmanifest',
+  '/poan/icon.svg'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
