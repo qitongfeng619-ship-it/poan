c4a8d87bc3745451172c71fec7fb19a944e8a319
qitongfeng619-ship-it <qitongfeng619@gmail.com>
2026-07-31 15:14:20 +0800

    chore: bump SW to v3 to invalidate old dice.html cache

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE = 'poan-v2';
+const CACHE = 'poan-v3';
 const ASSETS = [
   '/poan/index.html',
   '/poan/pools.html',
+  '/poan/done-list.html',
   '/poan/stories.html',
   '/poan/dice.html',
   '/poan/bodymap.html',
-  '/poan/done-list.html',
-  '/poan/manifest.webmanifest',
-  '/poan/icon.svg'
+  '/poan/icon.svg',
+  '/poan/manifest.webmanifest'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
